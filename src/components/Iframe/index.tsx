748901d225dc94ed3be2fabe14bdbc6626484ab5
AlekseevKV <Kalekseev@ibs.ru>
2024-06-02 22:00:08 +0300

    deleted styles from iframe

--- src/components/Iframe/index.tsx
+++ src/components/Iframe/index.tsx
@@ -1,6 +1,5 @@
 ﻿import { createPortal } from 'react-dom';
 import styles from './styles.module.scss';
-import { useEffect } from 'react';
 
 type Props = {
   isShow: boolean;
@@ -10,25 +9,6 @@ type Props = {
 
 const Iframe = ({ isShow, onClose, link }: Props): JSX.Element | null => {
   const className = isShow ? `${styles.modal} ${styles.show}` : styles.modal;
-
-  const addStyles = (ev: any) => {
-    const iframeTarget = ev.target as HTMLIFrameElement;
-    // const newStyle = document.createElement('style');
-    // newStyle.textContent = `body {font-size: 16px !important;} div.wtq-body {height: 100 !important; overflow-y: auto !important;} div.wtq-q-itms-area {
-    //   height: auto !important;} div.wtq-q-footer {position: relative !important;}`;
-
-    // iframeTarget.contentDocument?.head.appendChild(newStyle);
-  };
-
-  useEffect(() => {
-    const iframe = document.getElementById('iframe-assessment');
-    if (iframe) {
-      iframe.addEventListener('load', addStyles);
-    }
-    return () => {
-      window.removeEventListener('load', addStyles);
-    };
-  }, [addStyles, isShow]);
 
   if (!isShow) {
     return null;
